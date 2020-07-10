09ef4e5120383a1ce16d57bcd8ac282c5b3dc459
isik-tech <68105380+isik-tech@users.noreply.github.com>
2020-07-10 11:58:24 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+var input = document.getElementById("name");
+var birthday = document.getElementById("birthday");
+var button = document.getElementById("enter");
+
+var ul = document.querySelector("ul");
+
+
+function inputLength(){
+    return input.value.length;
+}
+
+
+
+function createListElement(){
+        var li = document.createElement("li");
+        li.appendChild(document.createTextNode(input.value));
+        ul.appendChild(li);
+        input.value = "";
+    }
+
+
+function addListAfterClick(){
+    if(inputLength() > 0){
+        createListElement();
+    }
+}
+
+
+
+function addListAfterKeypress(event){
+    if(inputLength() > 0 && event.keyCode === 13){
+        createListElement();
+    }
+}
+
+
+button.addEventListener("click", addListAfterClick);
+input.addEventListener("keypress", addListAfterKeypress);
+birthday.addEventListener("click", addListAfterClick)
